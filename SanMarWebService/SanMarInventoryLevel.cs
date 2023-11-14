4e333f4d96d909577dbc815d4a23065d8efed6b5
Chris Laforet <laforet@chrislaforetsoftware.com>
2023-11-14 14:23:09 -0500

    Initial plumbing of SanMar SOAP Web Service

---
+++ SanMarWebService/SanMarInventoryLevel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SanMarWebService
+{
+	public class SanMarInventoryLevel
+	{
+		public string Style { get; set; }
+		public string Color { get; set; }
+		public string Size { get; set; }
+		public long? Quantity { get; set; }
+	}
+}
---
+++ SanMarWebService/SanMarServices.cs
@@ -0,0 +1,21 @@
+﻿namespace SanMarWebService
+{
+	public class SanMarServices
+	{
+		private readonly string customerNumber;
+		private readonly string userName;
+		private readonly string password;
+
+		public SanMarServices(string customerNumber, string userName, string password)
+		{
+			this.customerNumber = customerNumber;
+			this.userName = userName;
+			this.password = password;
+		}
+
+		public List<SanMarInventoryLevel> GetInventoryLevelsFor(string Style, string Color = null, string Size = null)
+		{
+
+		}
+	}
+}
