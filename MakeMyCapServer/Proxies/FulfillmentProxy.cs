0c7e69ff6f91cd8cfdb4424544a4597c137ce293
Chris Laforet <laforet@chrislaforetsoftware.com>
2023-11-21 09:58:39 -0500

    Wired proxy to support shopify order handling

---
+++ MakeMyCapServer/Proxies/FulfillmentProxy.cs
@@ -0,0 +1,45 @@
+﻿using MakeMyCapServer.Model;
+
+namespace MakeMyCapServer.Proxies;
+
+public class FulfillmentProxy : IFulfillmentProxy
+{
+	private readonly MakeMyCapServerContext context;
+	
+	public FulfillmentProxy(MakeMyCapServerContext context)
+	{
+		this.context = context;
+	}
+	
+	public Order? GetOrderById(long orderId)
+	{
+		return context.Orders.Find(orderId);
+	}
+
+	public void SaveOrder(Order order)
+	{
+		if (!context.Orders.Contains(order))
+		{
+			context.Orders.Add(order);
+		}
+		context.SaveChanges();
+	}
+
+	public void SaveFullfillmentOrder(FulfillmentOrder fulfillmentOrder)
+	{
+		if (!context.FulfillmentOrders.Contains(fulfillmentOrder))
+		{
+			context.FulfillmentOrders.Add(fulfillmentOrder);
+		}
+		context.SaveChanges();	
+	}
+
+	public void SaveOrderLineItem(OrderLineItem orderLineItem)
+	{
+		if (!context.OrderLineItems.Contains(orderLineItem))
+		{
+			context.OrderLineItems.Add(orderLineItem);
+		}
+		context.SaveChanges();
+	}
+}
---
+++ MakeMyCapServer/Proxies/IFulfillmentProxy.cs
@@ -0,0 +1,13 @@
+﻿using MakeMyCapServer.Model;
+
+namespace MakeMyCapServer.Proxies;
+
+public interface IFulfillmentProxy
+{
+	Order? GetOrderById(long orderId);
+	void SaveOrder(Order order);
+
+	void SaveFullfillmentOrder(FulfillmentOrder fulfillmentOrder);
+
+	void SaveOrderLineItem(OrderLineItem orderLineItem);
+}
