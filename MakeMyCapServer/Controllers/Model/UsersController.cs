b3014ee294721a832777d7690bab0697b4289bdb
Chris <laforet@chrislaforetsoftware.com>
2024-06-13 22:24:04 -0400

    Building out controllers

---
+++ MakeMyCapServer/Controllers/Model/UsersController.cs
@@ -0,0 +1,23 @@
+﻿using MakeMyCapServer.CQS.CommandHandler;
+using MakeMyCapServer.CQS.QueryHandler;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MakeMyCapServer.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class UsersController : ControllerBase
+{
+	private readonly UserQueryHandler UserQueryHandler;
+	private readonly CreateUserCommandHandler CreateUserCommandHandler;
+	private readonly ILogger<UsersController> logger;
+
+	public UsersController(IServiceProvider serviceProvider, ILogger<UsersController> logger)
+	{
+		this.logger = logger;
+		
+		UserQueryHandler = ActivatorUtilities.CreateInstance<UserQueryHandler>(serviceProvider);
+		CreateUserCommandHandler = ActivatorUtilities.CreateInstance<CreateUserCommandHandler>(serviceProvider);
+	}
+
+}
---
+++ MakeMyCapServer/Controllers/SettingsController.cs
@@ -0,0 +1,37 @@
+﻿using MakeMyCapServer.CQS.CommandHandler;
+using MakeMyCapServer.CQS.QueryHandler;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MakeMyCapServer.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class SettingsController : ControllerBase
+{
+	private readonly SettingsQueryHandler SettingsQueryHandler;
+	private readonly DistributorsQueryHandler DistributorsQueryHandler;
+	private readonly DistributorSkusQueryHandler DistributorSkusQueryHandler;
+	private readonly SkuQueryHandler SkuQueryHandler;
+	private readonly NotificationsQueryHandler NotificationsQueryHandler;
+
+	private readonly ChangeSettingsCommandHandler ChangeSettingsCommandHandler;
+	private readonly CreateSkuCommandHandler CreateSkuCommandHandler;
+	private readonly ChangeNotificationsCommandHandler ChangeNotificationsCommandHandler;
+	private readonly ILogger<SettingsController> logger;
+
+	public SettingsController(IServiceProvider serviceProvider, ILogger<SettingsController> logger)
+	{
+		this.logger = logger;
+		
+		SettingsQueryHandler = ActivatorUtilities.CreateInstance<SettingsQueryHandler>(serviceProvider);
+		DistributorSkusQueryHandler = ActivatorUtilities.CreateInstance<DistributorSkusQueryHandler>(serviceProvider);
+		DistributorsQueryHandler = ActivatorUtilities.CreateInstance<DistributorsQueryHandler>(serviceProvider);
+		SkuQueryHandler = ActivatorUtilities.CreateInstance<SkuQueryHandler>(serviceProvider);
+		NotificationsQueryHandler = ActivatorUtilities.CreateInstance<NotificationsQueryHandler>(serviceProvider);
+
+		ChangeSettingsCommandHandler = ActivatorUtilities.CreateInstance<ChangeSettingsCommandHandler>(serviceProvider);
+		CreateSkuCommandHandler = ActivatorUtilities.CreateInstance<CreateSkuCommandHandler>(serviceProvider);
+		ChangeNotificationsCommandHandler = ActivatorUtilities.CreateInstance<ChangeNotificationsCommandHandler>(serviceProvider);
+	}
+
+}
